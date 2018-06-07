96bae6fc415d2044e00c3df85ee58966f345cee9
Raghu Betina <raghu@firstdraft.com>
2018-06-07 04:00:05 +0000

    Direct association between bets and follows

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,6 +1,9 @@
 class Bet < ApplicationRecord
   # Direct associations
 
+  has_many   :follows,
+             :dependent => :destroy
+
   belongs_to :bet_starter,
              :class_name => "User",
              :foreign_key => "owner",
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,11 @@
 class Follow < ApplicationRecord
   # Direct associations
 
+  belongs_to :single_bet,
+             :class_name => "Bet",
+             :foreign_key => "bet",
+             :counter_cache => true
+
   belongs_to :participant,
              :class_name => "User",
              :foreign_key => "participants",
